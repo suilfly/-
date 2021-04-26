6a5610c0ae40f15a751d451a2fdb4379ecaba59f
suilfly <876794103@qq.com>
2021-04-26 10:35:27 +0800

    补充注释

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,7 +1,7 @@
 import Header from '../components/header';
 import '../assets/js/imports'
 (function(doc){
-
+//立即执行函数，形成闭包
     //获取index.html中#app结点，并把Header组件挂载渲染上
     const mApp = doc.querySelector('#app');
     const init = function(){
